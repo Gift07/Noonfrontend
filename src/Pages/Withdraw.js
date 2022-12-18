c5bad56c2121771c1d26dc30ff6b6bafd5ff9fdd
Gift07 <giftedwards9@gmail.com>
2022-12-19 00:08:30 +0300

    updated

--- src/Pages/Withdraw.js
+++ src/Pages/Withdraw.js
@@ -25,7 +25,7 @@ const Withdraw = () => {
       balance: account.balance,
       account_name: account.withdraw_account.account_name,
       account_type: account.withdraw_account.account_type,
-      account_number: account.withdraw_account.account_info,
+      account_info: account.withdraw_account.account_info,
     };
     console.log(body);
     dispatch(CreateWithdraw({ axiosPrivate, body }));
